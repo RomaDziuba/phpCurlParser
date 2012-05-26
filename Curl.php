28bbca6d81b3c3ca134b98506ca52f76c7353dd7
Denis Panaskin <goliathdp@gmail.com>
2012-05-26 13:25:46 -0400

    Add sock proxy support

--- Curl.php
+++ Curl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class Curl extends EventDispatcher
 {
@@ -7,26 +7,26 @@ class Curl extends EventDispatcher
     protected $lists;
     protected $lastIndex = 0;
     protected $master;
-    
-    
+
+
     public function __construct($options = array())
     {
         if (!extension_loaded('curl')) {
             throw new Exception(_('cURL extension is not available on your server'));
         }
-        
+
         $this->options = $options;
-        
+
     }
-    
-    public function getUrl($url, $pos_params = false, $proxy = false, $proxy_pswd = false, $isIgnoreErrors = false) 
+
+    public function getUrl($url, $pos_params = false, $proxy = false, $proxy_pswd = false, $isIgnoreErrors = false)
     {
     	$this->resource = curl_init();
-        
+
         $this->options[CURLOPT_URL] = $url;
-        
+
         if ($proxy) {
-        	
+
         	if (is_array($proxy)) {
         		$this->options[CURLOPT_PROXYTYPE] = $proxy[0];
         		$this->options[CURLOPT_PROXY] = $proxy[1];
@@ -34,40 +34,42 @@ class Curl extends EventDispatcher
         		$this->options[CURLOPT_PROXY] = $proxy;
         	}
         }
-        
+
         if($proxy_pswd) {
             $this->options[CURLOPT_PROXYUSERPWD] = $proxy_pswd;
         }
-        
+
         if($pos_params) {
             $this->options[CURLOPT_POST] = true;
             $this->options[CURLOPT_POSTFIELDS] = is_array($pos_params) ?  join("&", $pos_params) : $pos_params;
         }
-        
+
         curl_setopt_array($this->resource, $this->options);
-        
+
         $res = curl_exec($this->resource);
         $code = curl_getinfo($this->resource, CURLINFO_HTTP_CODE);
-        
+
         if($code != 200 && !$isIgnoreErrors) {
             return false;
         }
-    
+
         return  $res;
     } // end getUrl
-    
+
     public function addUrl($url, $externalOptions = array(), $pos_params = false, $proxy = false, $proxy_pswd = false, $isIgnoreErrors = false)
     {
+    	$index = $this->lastIndex;
+
     	$resource = &$this->resource[$this->lastIndex];
     	$resource = curl_init();
-    	
+
     	$options = $this->options;
     	foreach ($externalOptions as $key => $value) {
     		$options[$key] = $value;
     	}
-    	
+
     	$options[CURLOPT_URL] = $url;
-    	
+
     	if ($proxy) {
     		if (is_array($proxy)) {
     			$options[CURLOPT_PROXYTYPE] = $proxy[0];
@@ -76,26 +78,27 @@ class Curl extends EventDispatcher
     			$options[CURLOPT_PROXY] = $proxy;
     		}
     	}
-    	
+
     	if($proxy_pswd) {
     		$options[CURLOPT_PROXYUSERPWD] = $proxy_pswd;
     	}
-    	
+
     	if($pos_params) {
     		$options[CURLOPT_POST] = true;
     		$options[CURLOPT_POSTFIELDS] = is_array($pos_params) ?  join("&", $pos_params) : $pos_params;
     	}
-    	
+
     	curl_setopt_array($resource, $options);
-    	
+
     	$this->lastIndex++;
-    	
+
+    	return $index;
     } // end addUrl
-    
+
     public function start()
     {
     	$this->master = curl_multi_init();
-    	
+
     	foreach ($this->resource as $index => $resource) {
     		curl_multi_add_handle($this->master, $resource);
     	}
@@ -104,22 +107,22 @@ class Curl extends EventDispatcher
     		//usleep(10000);
     		$mrc = curl_multi_exec($this->master, $running);
     	} while ($running > 0);
-    	
+
     	$result = array();
-    	
+
     	foreach ($this->resource as $index => $resource) {
     		$result[$index] = curl_multi_getcontent($resource);
     	}
-    	
+
     	foreach ($this->resource as $index => $resource) {
     		curl_multi_remove_handle($this->master, $resource);
     	}
-    	
+
     	curl_multi_close($this->master);
-    	
+
     	return $result;
     }
-    
-    
+
+
 }
 ?>
